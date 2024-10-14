742828c421a3d5705529e8aadcc28e8649da9e05
Nikita Kaberov <enagtim@mail.ru>
2024-10-14 15:26:43 +0300

    Приложение настроено и готово к работе

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "localhost",
+    port: 3001,
+    open: true,
+    watch: {
+      usePolling: true,
+    },
+    hmr: {
+      protocol: "ws",
+      host: "localhost",
+    },
+  },
+});
